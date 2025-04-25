6865fb4b4356ebc64e4f60524befb2d45d7818c4
Toon Verwerft <toonverwerft@gmail.com>
2025-04-25 12:53:19 +0200

    Use new Bundle extension structure

--- src/DbalToolsBundle.php
+++ src/DbalToolsBundle.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace Phpro\DbalTools;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 final class DbalToolsBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $configDir = __DIR__ . '/../config';
+
+        $container->import($configDir . '/commands.xml');
+        $container->import($configDir . '/fixtures.xml');
+        $container->import($configDir . '/migrations.xml');
+        $container->import($configDir . '/schema.xml');
+    }
 }
