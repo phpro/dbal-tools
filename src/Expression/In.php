f18a23e5f741cabc11bcb034518761d69fe620f7
Youri Van Laer <youri.vanlaer@phpro.be>
2025-05-14 15:27:22 +0200

    Add static method fromIterable to In expression class

--- src/Expression/In.php
+++ src/Expression/In.php
@@ -4,12 +4,32 @@ declare(strict_types=1);
 
 namespace Phpro\DbalTools\Expression;
 
+use function Psl\Vec\map;
+
 final readonly class In implements Expression
 {
     public function __construct(
         private Expression $subject,
         private Expressions $in,
     ) {
+    }
+
+    /**
+     * @template Tk
+     * @template Tv
+     *
+     * @param iterable<Tk, Tv>          $iterable
+     * @param (Closure(Tv): Expression) $function
+     */
+    public static function fromIterable(
+        Expression $subject,
+        iterable $iterable,
+        \Closure $function,
+    ): self {
+        return new self(
+            $subject,
+            new Expressions(...map($iterable, $function)),
+        );
     }
 
     public function toSQL(): string
