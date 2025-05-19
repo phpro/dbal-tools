58f32142e25d390ad558c43765b5455553f35bf1
Toon Verwerft <toonverwerft@gmail.com>
2025-05-19 07:43:08 +0200

    Add literal string exists assertions

--- src/Test/Assert/DbalAssert.php
+++ src/Test/Assert/DbalAssert.php
@@ -5,12 +5,24 @@ declare(strict_types=1);
 namespace Phpro\DbalTools\Test\Assert;
 
 use Doctrine\DBAL\Connection;
+use Phpro\DbalTools\Expression\Comparison;
 use Phpro\DbalTools\Expression\Expression;
+use Phpro\DbalTools\Expression\LiteralString;
 use PHPUnit\Framework\Assert;
 
 trait DbalAssert
 {
     abstract protected static function connection(): Connection;
+
+    public static function assertLiteralValueExists(string $table, Expression $field, string $literalValue): void
+    {
+        self::assertRecordExists($table, Comparison::equal($field, new LiteralString($literalValue)));
+    }
+
+    public static function assertLiteralValueDoesNotExist(string $table, Expression $field, string $literalValue): void
+    {
+        self::assertRecordDoesNotExist($table, Comparison::equal($field, new LiteralString($literalValue)));
+    }
 
     public static function assertRecordExists(string $table, Expression $expression): void
     {
